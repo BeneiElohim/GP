4aea0a169e65b4d04a74dd108fbc15e56e2aeb7c
ismailtarihc <0105mert@gmail.com>
2023-02-19 20:29:37 +0300

    main player class variables

--- player.js
+++ player.js
@@ -1,4 +1,11 @@
 var player = {
-    name: "Marta",
-    lastName: "Silva",
+    name: "Roger Glubbie Blubbie",
+    nickname: "RGB Boy",
+    xPos: 0,
+    yPos: 0,
+    isLeft: false,
+    isRight: false,
+    isFalling: false,
+    isPlummeting: false,
+
 };
