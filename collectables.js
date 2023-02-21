a8eabcaefd7c20e06ca2864dedd98d4df63e5214
ismailtarihc <0105mert@gmail.com>
2023-02-21 18:31:25 +0300

    collectables reshape

--- collectables.js
+++ collectables.js
@@ -9,11 +9,11 @@ function Collectable(x,y, isFound) {
             fill(255);
             triangle(this.x, 30+this.y, 40+this.x, 30+this.y, 20+this.x, 0+this.y); //BG Triangle
             fill(0,0,255); 
-            triangle(78+this.x, 30+this.y, 122+this.x, 30+this.y, 100+this.x, 0+this.y); //Top One Triangle
+            triangle(this.x+10, 14+this.y, 30+this.x, 14+this.y, 20+this.x, this.y ); //Top One Triangle
             fill(255,0,0);
-            triangle(this.x, 30+this.y, 20+this.x, 30+this.y, 10+this.x, 40+this.y); //Bottom left
-            fill(0,255,0,170);
-            triangle(140+this.x, 54+this.y, 118+this.x, 80+this.y, 160+this.x, 80+this.y); //Bottom Right  
+            triangle(this.x, 30+this.y, 18+this.x, 30+this.y, 10+this.x, 14+this.y); //Bottom left
+            fill(0,255,0);
+            triangle(this.x + 20, 30+this.y, 40+this.x, 30+this.y, 30+this.x, 14+this.y); //Bottom Right  
         }
     }
     this.checkContact = function() {
